c197f91b7e82b11efaf7f86f7536aee5df5529dc
Dennis Hoyer <mail@dennishoyer.com>
2023-02-06 18:45:40 +0100

    Improves code readability

--- src/hooks/useAIOpponent.js
+++ src/hooks/useAIOpponent.js
@@ -3,11 +3,9 @@ import { useEffect, useState } from "react"
 export const useAIOpponent = turn => {
     const[aiChoice, setAIChoice] = useState('');
     useEffect(() => {
-        if (turn === 1){
-            const options = ['attack', 'magic', 'heal']
-            setAIChoice(options[Math.floor(Math.random()* options.legnth)]);
-        }
-
+        if (turn !== 1) return;
+        const options = ['attack', 'magic', 'heal']
+        setAIChoice(options[Math.floor(Math.random()* options.legnth)]);
     }, [turn]);
 
     return aiChoice;
